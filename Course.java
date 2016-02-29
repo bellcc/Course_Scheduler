8f608d7a8d8e3852bb9d86f54fc2af5e8d971ebb
chris <bellcc@miamioh.edu>
2016-02-29 16:29:05 -0500

    Course Parser

---
+++ Course.java
@@ -0,0 +1,266 @@
+
+public class Course {
+
+	String course_num;
+	String course;
+	String hours;
+	String enrollment;
+	String max_enrollment;
+	String start_time;
+	String end_time;
+	String room;
+	String meeting_category;
+
+	String subject;
+	String section;
+	String title;
+	String days;
+	String building;
+	String instructor;
+	String meeting_type;
+
+	public Course() {
+
+		course_num = "";
+		course = "";
+		hours = "";
+		enrollment = "";
+		max_enrollment = "";
+		start_time = "";
+		end_time = "";
+		room = "";
+		meeting_category = "";
+
+		subject = "";
+		section = "";
+		title = "";
+		days = "";
+		building = "";
+		instructor = "";
+		meeting_type = "";
+
+	}
+
+	public Course(String crn, String co, String hr, String en, String me, String st, String et, String rm, String mc, String sb, String se, String ti, String ds, String bu, String in, String mt) {
+
+		course_num = crn;
+		course = co;
+		hours = hr;
+		enrollment = en;
+		max_enrollment = me;
+		start_time = st;
+		end_time = et;
+		room = rm;
+		meeting_category = mc;
+
+		subject = sb;
+		section = se;
+		title = ti;
+		days = ds;
+		building = bu;
+		instructor = in;
+		meeting_type = mt;
+
+	}
+
+	public void setCourseNumber(String crn) {
+
+		course_num = crn;
+
+	}
+
+	public void setCourse(String cr) {
+
+		course = cr;
+
+	}
+
+	public void setHours(String hr) {
+
+		hours = hr;
+
+	}
+
+	public void setEnrollment(String en) {
+
+		enrollment = en;
+
+	}
+
+	public void setMaxEnrollment(String me) {
+
+		max_enrollment = me;
+
+	}
+
+	public void setStartTime(String st) {
+
+		start_time = st;
+
+	}
+
+	public void setEndTime(String et) {
+
+		end_time = et;
+
+	}
+
+	public void setRoom(String rm) {
+
+		room = rm;
+
+	}
+
+	public void setMeetingCategory(String mc) {
+
+		meeting_category = mc;
+
+	}
+
+	public void setSubject(String sb) {
+
+		subject = sb;
+
+	}
+
+	public void setSection(String se) {
+
+		section = se;
+
+	}
+
+	public void setTitle(String ti) {
+
+		title = ti;
+
+	}
+
+	public void setDays(String ds) {
+
+		days = ds;
+
+	}
+
+	public void setBuilding(String bu) {
+
+		building = bu;
+
+	}
+
+	public void setInstructor(String in) {
+
+		instructor = in;
+
+	}
+
+	public void setMeetingType(String mt) {
+
+		meeting_type = mt;
+
+	}
+
+	public String getCourseNum() {
+
+		return course_num;
+
+	}
+
+	public String getCourse() {
+
+		return course;
+
+	}
+
+	public String getHours() {
+
+		return hours;
+
+	}
+
+	public String getEnrollment() {
+
+		return enrollment;
+
+	}
+
+	public String getMaxEnrollment() {
+
+		return max_enrollment;
+
+	}
+
+	public String getStartTime() {
+
+		return start_time;
+
+	}
+
+	public String getEndTime() {
+
+		return end_time;
+
+	}
+
+	public String getRoom() {
+
+		return room;
+
+	}
+
+	public String getMeetingCategory() {
+
+		return meeting_category;
+
+	}
+
+	public String getSubject() {
+
+		return subject;
+
+	}
+
+	public String getSection() {
+
+		return section;
+
+	}
+
+	public String getTitle() {
+
+		return title;
+
+	}
+
+	public String getDays() {
+
+		return days;
+
+	}
+
+	public String getBuilding() {
+
+		return building;
+
+	}
+
+	public String getInstructor() {
+
+		return instructor;
+
+	}
+
+	public String getMeetingType() {
+
+		return meeting_type;
+
+	}
+
+	public String toString() {
+
+		String str = "Course Num: " + course_num + "\nSubject: " + subject + "\nCourse: " + course + "\nSection: " + section + "\nTitle:" + title + "\nHours: " + hours + "\nEnrollment: " + enrollment + "\nMax Enrollment: " + max_enrollment + "\nStart Time: " + start_time + "\nEnd Time: " + end_time + "\nDays: " + days + "\nBuilding: " + building + "\nRoom: " + room + "\nInstructor: " + instructor + "\nMeeting Type: " + meeting_type + "\nMeeting Category: " + meeting_category;
+
+		return str;
+
+	}
+
+}
---
+++ courseParser.java
@@ -0,0 +1,98 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public final class courseParser {
+
+	public static ArrayList<Course> parseFile(File file) throws FileNotFoundException {
+
+		ArrayList<Course> schedule = new ArrayList<Course>();
+
+		try {
+
+			Scanner scanner = new Scanner(file);
+
+			scanner.useDelimiter(",");
+
+			scanner.nextLine();
+
+			while(scanner.hasNext()) {
+
+				String course_str = scanner.nextLine();
+				String parts[] = course_str.split(",");
+
+				String parts_tmp[] = new String[16];
+				int counter = 0;
+
+				for(int i=0;i<parts.length;i++) {
+
+					if(parts[i].startsWith("\"")) {
+
+						int index = -1;
+
+						for(int j=i;j<parts.length;j++){
+
+							if(parts[j].endsWith("\"")) {
+								index = j;
+								break;
+
+							}
+
+						}
+
+						String concat_str = "";
+
+						if(i == index) {
+
+							concat_str = parts[i];
+
+						}
+						else{
+
+							concat_str = parts[i];
+
+							for(int j=i+1;j<=index;j++) {
+
+								concat_str += "," +  parts[j];
+
+							}
+
+						}
+
+						parts_tmp[counter] = concat_str;
+						counter++;
+						i = index;
+
+
+					}
+					else{
+
+						parts_tmp[counter] = parts[i];
+						counter++;
+
+					}
+
+				}
+
+				Course aCourse = new Course(parts_tmp[0], parts_tmp[2], parts_tmp[5], parts_tmp[6], parts_tmp[7], parts_tmp[8], parts_tmp[9], parts_tmp[12], parts_tmp[15], parts_tmp[1], parts_tmp[3], parts_tmp[4], parts_tmp[10], parts_tmp[11], parts_tmp[13], parts_tmp[14]);
+
+				schedule.add(aCourse);
+
+			}
+	
+			scanner.close();
+
+		}
+		catch (FileNotFoundException er) {
+
+			System.out.println("The course schedule file is missing.");
+
+		}
+
+		return schedule;
+
+	}
+
+}
